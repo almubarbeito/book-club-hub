bcb5991eaf0c4254b2b936b890f94bc58fb191ed
almubarbeito <almubarbeito@gmail.com>
2025-07-05 22:19:10 +0200

    chore: Add type declarations for CSS modules

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,8 @@
+// File: frontend/src/vite-env.d.ts
+
+/// <reference types="vite/client" />
+
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
